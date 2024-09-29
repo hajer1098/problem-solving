4869e0cccb9a6af45cdc165732f1543831fdedae
Hajer Boukhari <hajerboukhari@gmail.com>
2024-09-30 00:08:13 +0100

    palindrome-string-builder

---
+++ src/PalindromeStringBuilder.java
@@ -0,0 +1,13 @@
+//using StringBuilder
+class Palindrome {
+    
+    public static void main(String[] args) {
+        
+        String str="RADAR";
+       String reserved=new StringBuilder(str).reverse().toString();
+    System.out.println(reserved);
+     
+    }
+    
+    
+}
